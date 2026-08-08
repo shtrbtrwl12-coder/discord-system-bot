71788893e6c4a48f1d34d92d5bb6cd9a88f82535
shtrbtrwl12-coder <shtrbtrwl12@gmail.com>
2026-08-08 05:21:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 const { 
     Client, 
     GatewayIntentBits, 
